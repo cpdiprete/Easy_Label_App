bc3e54dfbc78431f04618c46df771c3d5179511d
Calvin DiPrete <cpdiprete@gmail.com>
2025-08-19 17:58:55 -0400

    Alot of progress on labeling screen, saving before connecting project and projects screens

--- src/app/project.tsx
+++ src/app/project.tsx
@@ -4,27 +4,44 @@ import { Link, router } from "expo-router";
 import { FlatList } from "react-native";
 import { useState } from "react";
 import React from "react"
-// What is the technical adequacy of the image?
+import { Dropdown } from 'react-native-element-dropdown';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { useLocalSearchParams } from "expo-router";
+type Prompt = { question: string; answers: string[] };
+
 const data = [
     {
-        Prompt: {
-            Question: "What is the technical adequacy of the image?",
-            Answers: ["Adequate", "Artifact", "Unreadable"]
-        }
-    },
-    {
-        Prompt: {
-            Question: "What type of tubule is this?",
-            Answers: ["Proximal", "Distal", "Other"]
-        }
-    },
-    {
-        Prompt: {
-            Question: "What type of tubule is this?",
-            Answers: ["Proximal", "Distal", "Other"]
-        }
+        title:"Testing",
+        admin:"Calvin",
+        organization:"Sigma Nu",
+        protocolpdf:"www.boom",
+        images: ["imageurl1", "imageurl2"],
+        prompts: [
+            {
+                prompt: {
+                    question: "What is the technical adequacy of the image?",
+                    answers: ["Adequate", "Artifact", "Unreadable"]
+                },
+            },
+            {
+                prompt: {
+                    question: "What type of tubule is this?",
+                    answers: ["Proximal", "Distal", "Other"]
+                },
+            },
+            {
+                prompt: {
+                    question: "What type of tubule is this?",
+                    answers: ["Proximal", "Distal", "Other"]
+                }
+            },
+        ]
     }
 ];
+
+
+
 
 const NoDropdownAnswers = ({prompt_index, question, answers}) => {
     const [selectedAnswer, setSelectedAnswer] = useState([null, null]);
@@ -40,7 +57,7 @@ const NoDropdownAnswers = ({prompt_index, question, answers}) => {
                 <TouchableOpacity
                     key={`${ans}-${i}`}
                     style= {
-                        selectedAnswer[0] === i ? styles.selectedAnswerChoice : styles.answerChoice
+                        selectedAnswer[0] === i ? styles.selectedAnswerChoiceBox : styles.answerChoiceBox
                     }
                     onPress={() => {
                         setSelectedAnswer([i, ans])
@@ -57,28 +74,79 @@ const NoDropdownAnswers = ({prompt_index, question, answers}) => {
     )
 }
 
+// const DrodownAnswers = ({prompt_index, question, answers}) => {
+//     const [selectedAnswer, setSelectedAnswer] = useState([null, null]);
+//     const [selectedRow, setSelectedRow] = useState([null, null]);
+//     const [openDropdown, setOpenDropdown] = useState(false)
+//     return (
+//         <View style = {(selectedRow[0] === prompt_index) ? styles.NonDropdownPromptAnswered : styles.NonDropdownPrompt}>
+//             {/* <Text style={styles.questionText}>{question}</Text> */}
+//             <Text>{question}</Text>
+//             <TouchableOpacity 
+//                 style={styles.answerChoiceBox}
+//                 onPress={() => {
+//                     setOpenDropdown(true)
+//                 }}
+//             >
+//                 {openDropdown ? (
+//                     <>
+//                         {/* <Text>{selectedAnswer[1]}</Text> */}
+//                         <Text>{selectedAnswer?.[1] ?? ""}</Text>
+//                         {answers.map((ans, i) => (
+//                             <TouchableOpacity
+//                                 key={`${ans}-${i}`}
+//                                 style= {
+//                                     selectedAnswer[0] === i ? styles.selectedAnswerChoiceBox : styles.answerChoiceBox
+//                                 }
+//                                 onPress={() => {
+//                                     setSelectedAnswer([i, ans])
+//                                     setSelectedRow([prompt_index, ans])
+//                                     setOpenDropdown(false)
+//                                 }}
+//                             >
+//                                 <Text style = {selectedAnswer[0] === i ? styles.selectedAnswerChoiceText : styles.answerChoiceText}>
+//                                     {ans}
+//                                 </Text>
+//                             </TouchableOpacity>
+//                         ))}
+//                     </>
+//                     ) : (
+//                         selectedAnswer[1] === null ? null : 
+//                         <View style={styles.selectedAnswerChoiceBox}> 
+//                             <Text style={{color:"white", textDecorationLine: 'underline'}}>{selectedAnswer[1]}</Text>
+//                         </View>
+//                     )
+//                 }
+//             </TouchableOpacity>
+//         </View>
+//     )
+// }
+
+
 const DrodownAnswers = ({prompt_index, question, answers}) => {
     const [selectedAnswer, setSelectedAnswer] = useState([null, null]);
     const [selectedRow, setSelectedRow] = useState([null, null]);
     const [openDropdown, setOpenDropdown] = useState(false)
     return (
-        <View style = {(selectedRow[0] === prompt_index) ? styles.NonDropdownPromptAnswered : styles.NonDropdownPrompt}>
-            {/* <Text style={styles.questionText}>{question}</Text> */}
+        // <View style = {(selectedRow[0] === prompt_index) ? styles.NonDropdownPromptAnswered : styles.NonDropdownPrompt}>
+
             <TouchableOpacity 
-                style={styles.answerChoice}
+                // style={styles.answerChoiceBox}
+                style = {(selectedRow[0] === prompt_index) ? styles.dropdownPromptAnswered : styles.dropDownPrompt}
                 onPress={() => {
                     setOpenDropdown(true)
                 }}
             >
+            <Text>{question}</Text>
                 {openDropdown ? (
                     <>
                         {/* <Text>{selectedAnswer[1]}</Text> */}
-                        <Text>{selectedAnswer?.[1] ?? ""}</Text>
+                        {/* <Text>{selectedAnswer?.[1] ?? ""}</Text> */}
                         {answers.map((ans, i) => (
                             <TouchableOpacity
                                 key={`${ans}-${i}`}
                                 style= {
-                                    selectedAnswer[0] === i ? styles.selectedAnswerChoice : styles.answerChoice
+                                    selectedAnswer[0] === i ? styles.selectedAnswerChoiceBox : styles.answerChoiceBox
                                 }
                                 onPress={() => {
                                     setSelectedAnswer([i, ans])
@@ -93,11 +161,14 @@ const DrodownAnswers = ({prompt_index, question, answers}) => {
                         ))}
                     </>
                     ) : (
-                        selectedAnswer[1] === null ? <Text>{question}</Text> : <Text>{selectedAnswer[1]}</Text>
+                        selectedAnswer[1] === null ? null : 
+                        <View style={styles.selectedAnswerChoiceBox}> 
+                            <Text style={{color:"white", textDecorationLine: 'underline'}}>{selectedAnswer[1]}</Text>
+                        </View>
                     )
                 }
             </TouchableOpacity>
-        </View>
+        // </View>
     )
 }
 
@@ -105,36 +176,67 @@ const QuestionList = ({questionJSON}) => {
     const [selectedByQuestion, setSelectedByQuestion] = useState({});
     return (
         <FlatList 
+            style={styles.questionList}
             data={questionJSON}
             keyExtractor={(_, index) => String(index)}
             renderItem={({index, item }) => (
-                // <NoDropdownAnswers
-                //     prompt_index={index}
-                //     question = {item.Prompt.Question}
-                //     answers={item.Prompt.Answers}/>
+                <>
+                <NoDropdownAnswers
+                    prompt_index={index}
+                    question = {item.prompt.question}
+                    answers={item.prompt.answers}/>
                 <DrodownAnswers
                     prompt_index={index}
-                    question = {item.Prompt.Question}
-                    answers={item.Prompt.Answers}
+                    question = {item.prompt.question}
+                    answers={item.prompt.answers}
                 />
+                {/* <ImportedDropdown
+                //     prompt_index={index}
+                //     question = {item.Prompt.Question}
+                //     answers={item.Prompt.Answers}
+                // /> */}
+                </>
             )}
         >
         </FlatList>
     )
 }
 
+const SubmitButton = () => {
+    const [allowSubmission, setAllowSubmission] = useState(false);
+    return (
+        <TouchableOpacity
+        style={[(allowSubmission ? {backgroundColor:"orange"} : {backgroundColor:"gray"}), {width:"70%", alignSelf:"center", alignItems:"center", padding:4, marginTop:4, borderRadius:20}]}
+            onPress={() => {
+                setAllowSubmission(!allowSubmission)
+            }}
+        >
+            <Text>Submit</Text>
+        </TouchableOpacity>
+    )
+}
+
 const ProjectScreen = () => {
+    let ProjectParams = useLocalSearchParams();
+    let parsedPrompts: Prompt[] = JSON.parse(String(ProjectParams.prompts))
+    console.log(parsedPrompts)
     return (
         <View style={styles.projectScreenLayout}>
-            <View>
-                <Text> Title of the project </Text>
-                <Text> Organization admin </Text>
+            
+            <Button 
+            title="show message"
+                onPress={() => console.log(ProjectParams)}
+            ></Button>
+            <View style={styles.content}>
+                <Text style={styles.title}> Title: {data[0].title} </Text>
+                <Text style={styles.admin}> Admin: {data[0].admin} </Text>
                 <View
                     style={styles.imageBackground}>
                 </View>
-                <QuestionList questionJSON={data}/>
+                <QuestionList questionJSON={data[0].prompts}/>
             </View>
-            <Button title="Submit"></Button>
+            <SubmitButton/>
+            {/* <Button title="Submit"></Button> */}
         </View>
         
         )
@@ -142,31 +244,61 @@ const ProjectScreen = () => {
 
 const styles = StyleSheet.create({
     projectScreenLayout: {
+        // width:"100%",
         backgroundColor: 'lightblue',
+        flex: 1,
         // justifyContent: "center",
         alignItems: "center"
+    },
+    content: {
+        alignSelf: 'stretch',    // override parent centering and STRETCH
+        // or width: '100%'
+        paddingHorizontal: 16,
     },
     imageBackground: {
         backgroundColor: 'lightpink',
         width: 150,
-        height: 150
+        height: 150,
+        marginBottom:10,
+        alignSelf:"center"
     },
     NonDropdownPrompt: {
         backgroundColor: 'gray',
-        padding: 10,
-        marginVertical: 8,
-        marginHorizontal: 16,
+        padding: 4,
+        marginBottom: 10,
+        marginHorizontal: 0,
     }, 
     NonDropdownPromptAnswered: {
         backgroundColor: '#f9c2ff',
         borderWidth: 3,
         borderColor:'black',
-        padding: 10,
-        marginVertical: 8,
-        marginHorizontal: 16,
+        padding: 2,
+        marginBottom: 10,
+        marginHorizontal: 0,
+    },
+    dropDownPrompt: {
+        backgroundColor: 'gray',
+        paddingVertical: 10,
+        marginBottom: 10,
+        marginHorizontal: 0,
+    },
+    dropdownPromptAnswered: {
+        backgroundColor: '#f9c2ff',
+        borderWidth: 3,
+        borderColor:'black',
+        padding: 2,
+        marginBottom:10,
+        marginHorizontal: 0,
     },
     title: {
-        fontSize: 32,
+        fontSize: 24,
+        textAlign:"center",
+        alignSelf:"center"
+    },
+    admin: {
+        fontSize: 18,
+        textAlign:"center",
+        alignSelf:"center"
     },
     questionText: {
         fontSize: 16,
@@ -178,35 +310,64 @@ const styles = StyleSheet.create({
     choicesRow: {
         flexDirection: 'row',      // put children on the same line
         alignItems: 'center',
+        // margin:10
 
         // flexWrap: 'wrap',        // <-- uncomment if you want wrapping to the next line
         // gap: 8,                  // RN ≥0.71; otherwise use marginRight on child
     },
-    answerChoice: {
+    answerChoiceBox: {
         backgroundColor: 'white',
-        paddingHorizontal: 12,
-        paddingVertical: 8,
+        paddingHorizontal: 6,
+        paddingVertical: 4,
         borderRadius: 8,
         marginRight: 8,            // spacing if you don't use `gap`
-        marginTop: 8,              // nice when wrapping
+        marginTop: 4,              // nice when wrapping
+        marginBottom: 10, 
         alignSelf: 'flex-start',   // prevents stretching
     },
-    selectedAnswerChoice: {
+    selectedAnswerChoiceBox: {
         backgroundColor: 'black',
-        
-        paddingHorizontal: 12,
-        paddingVertical: 8,
+        paddingHorizontal: 8,
+        paddingVertical: 6,
         borderRadius: 8,
         marginRight: 8,            // spacing if you don't use `gap`
-        marginTop: 8,              // nice when wrapping
+        marginTop: 4,              // nice when wrapping
         alignSelf: 'flex-start',   // prevents stretching
     },
     answerChoiceText: {
-        color: "black"
+        color: "black",
     },
     selectedAnswerChoiceText: {
-        color:"white"
+        color:"white",
+        textDecorationLine: 'underline'
+    }, 
+    dropdown: {
+        backgroundColor: "purple",
+        // width:"100%",
+        alignSelf: 'stretch',
+        margin: 16,
+        height: 50,
+        borderBottomColor: 'gray',
+        borderBottomWidth: 0.5,
     },
+    icon: {
+        marginRight: 5,
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    questionList: {
+        // margin:30,
+        alignSelf: 'stretch',
+        backgroundColor:"lightblue"
+    }
 
 })
 export default ProjectScreen
--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -1,5 +1,10 @@
-import { Text, View, ScrollView, StyleSheet, Button } from "react-native";
+import { Text, View, ScrollView, StyleSheet, Image, Button, ImageSourcePropType } from "react-native";
 import { Link, router } from "expo-router";
+import { useRoute } from "@react-navigation/native"
+import { useState } from "react";
+
+type Prompt = { question: string; answers: string[] };
+type PromptWrapper = { prompt: Prompt };
 
 // Define a type for props
 type ProjectProperties = {
@@ -7,9 +12,79 @@ type ProjectProperties = {
     organization: string;
     contact: string;
     description: string;
+    images: ImageSourcePropType[];
+    prompts: PromptWrapper[];
 };
 
-const Project = ({title, organization, contact, description} : ProjectProperties) => {
+const data = [
+    { 
+        projects: [
+            {
+                title:"Testing",
+                admin:"Calvin",
+                organization:"Sigma Nu",
+                protocolpdf:"www.boom",
+                images: [require("../../assets/tubule_for_Calvin/1.png")],
+                description:"This project is about blah blah and mor eblh and some other blah and blah and duuuude b;ah blah",
+                prompts: [
+                    {
+                        prompt: {
+                            question: "What is the technical adequacy of the image?",
+                            answers: ["Adequate", "Artifact", "Unreadable"]
+                        },
+                    },
+                    {
+                        prompt: {
+                            question: "What type of tubule is this?",
+                            answers: ["Proximal", "Distal", "Other"]
+                        },
+                    },
+                    {
+                        prompt: {
+                            question: "What type of tubule is this?",
+                            answers: ["Proximal", "Distal", "Other"]
+                        }
+                    },
+                ]
+            },
+            {
+                title:"Project2",
+                admin:"fanfan@emory.edu",
+                organization:"Emory",
+                protocolpdf:"www.boom",
+                images: [require("../../assets/tubule_for_Calvin/2.png")],
+                description:"This is the 2nd project I want to test and see if this renders or not",
+                prompts: [
+                    {
+                        prompt: {
+                            question: "What is the technical adequacy of the image?",
+                            answers: ["Adequate", "Artifact", "Unreadable"]
+                        },
+                    },
+                    {
+                        prompt: {
+                            question: "What type of tubule is this?",
+                            answers: ["Proximal", "Distal", "Other"]
+                        },
+                    },
+                    {
+                        prompt: {
+                            question: "What type of tubule is this?",
+                            answers: ["Proximal", "Distal", "Other"]
+                        }
+                    },
+                ]
+            }
+        ],
+    }
+];
+
+
+
+const Project = ({title, organization, contact, description, prompts, images} : ProjectProperties) => {
+    // console.log("images = ", images)
+    // const [selectedProject, SetSelectedProject] = useState(["", "", "", "", "", [""]]);
+    // const path = "../.." + String(images[0])
     return (
         <View
         style={styles.project}>
@@ -17,13 +92,22 @@ const Project = ({title, organization, contact, description} : ProjectProperties
             <Text style={styles.orangeText}>Contact = {contact}</Text>
             <View
                 style={styles.imageBackground}>
+                    <Image
+                    // source={require(path)}
+                    source ={images[0]}
+                    // style={{width:"50%"}}
+                    >
+                        
+                    </Image>
             </View>
             {/* <Link></Link> */}
             {/* <Text>Description of the project blah blaj blah Description of the project blah blaj blah Description of the project blah blaj blah</Text> */}
-            <Text>{description}</Text>
+            <Text style={{marginVertical:5, marginHorizontal:10, alignContent:"center"}}>{description}</Text>
             <Button title="Continue"
                 onPress={() => {
-                    router.push("/project")
+                    // SetSelectedProject([title, organization, contact, description, images])
+                    console.log("PROMPTS BEING PASSED", JSON.stringify(prompts))
+                    router.push({pathname: "/project", params: {title: title, organization: organization, contact: contact, description:description, prompts: JSON.stringify(prompts)}})
                 }}>
             </Button>
         </View>
@@ -32,6 +116,7 @@ const Project = ({title, organization, contact, description} : ProjectProperties
 
 const ProjectScreen = () => {
     return (
+
         <ScrollView
             style={styles.scrollView}
                 contentContainerStyle={{
@@ -40,24 +125,18 @@ const ProjectScreen = () => {
                     alignItems: "center"  // horizontal centering
                 }}
             >
-            <Project
-                title="Project 1"
-                organization="Digital Pathology"
-                contact="fanfan@emory.edu"
-                description="Description of the project blah blaj blah Description of the project blah blaj blah Description of the project blah blaj blah"
-            />
-            <Project
-                title="Project 2"
-                organization="Georgia Tech"
-                contact="cdiprete6@gatech.edu"
-                description="Calvin likes NBA basketball, so he wants to make an nba statistical related project on kristaps porizingus stats compared to dirk nowitzki"
-            />
-            <Project
-                title="Project 3"
-                organization="Augusta university"
-                contact="706-814-1305"
-                description="lorem epsum snsjc sdcjnsdc svdjnsdvjd vdjnvdjfv"
-            />
+            {data[0].projects.map((proj, i) => (
+                <Project
+                    key={`${proj.title}-${i}`}
+                    title={proj.title}
+                    organization={proj.organization}
+                    contact={proj.admin}
+                    description={proj.description}
+                    images={proj.images}
+                    prompts={proj.prompts}
+                />
+            )
+            )}
         </ScrollView>
     );
 }
@@ -80,8 +159,9 @@ const styles = StyleSheet.create({
     },
     imageBackground: {
         backgroundColor: 'lightpink',
-        width: 150,
-        height: 150
+        // width: 150,
+        // height: 150.
+        flex: 1
     },
     orangeText: {
         color: 'darkorange',
